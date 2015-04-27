630c7c2d8c53ce9b75aeae2cb5c56e0b84b920f7
Eyal Arubas <eyalarubas@gmail.com>
2015-04-28 00:37:21 +0800

    add more UIBase class tests

--- test/01.uibase_test.js
+++ test/01.uibase_test.js
@@ -22,13 +22,43 @@ describe("UIBase class", function(){
 
     describe("building a UI component with hard coded html and css", function(){
 
-        var comp = new UIBase("<div></div>", "");
+        var comp = new UIBase("<div class='parent'><span class='child'>foo</span></div>", ".parent{.child{display:none;}}"),
+            $el;
 
-        it("should be ok", function(done){
-            comp.getElement().then(function($el){
-                console.log($el);
+        before(function(done){
+            comp.getElement().then(function(_$el){
+                _$el.appendTo("body");
+                $el = _$el;
                 done();
             });
+        });
+
+        describe("parent element", function(){
+
+            it("should be visible", function(){
+                expect($el.is(":visible")).to.be.true;
+            });
+
+            it("should be of type div", function(){
+                expect($el.is("div")).to.be.true;
+            });
+
+        });
+
+        describe("child element", function(){
+
+            it("should be visible", function(){
+                expect($el.find(".child").is(":visible")).to.be.false;
+            });
+
+            it("should be of type span", function(){
+                expect($el.find(".child").is("span")).to.be.true;
+            });
+
+            it("should contain the text 'foo'", function(){
+                expect($el.find(".child").text()).to.be.equal("foo");
+            });
+
         });
 
     });
