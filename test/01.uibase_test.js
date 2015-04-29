21c9a8174306d4db209b7ea977e2d53b34bc079e
Eyal Arubas <eyalarubas@gmail.com>
2015-04-29 23:28:01 +0800

    support for custom transform function for html...
    apply/remove components style on start/stop

--- test/01.uibase_test.js
+++ test/01.uibase_test.js
@@ -42,49 +42,240 @@ describe("UIBase class", function(){
 
     describe("building a UI component with hard coded html and LESS", function(){
 
-        var comp = new UIBase("<div class='parent'><span class='child'>foo</span></div>", ".parent{.child{display:none;}}"),
-            $el;
+        describe("using hard coded strings", function(){
 
-        before(function(done){
-            comp.getElement().then(function(_$el){
-                _$el.appendTo("body");
-                $el = _$el;
-                done();
+            var comp = new UIBase("<div class='parent'><span class='child'>foo</span></div>", ".parent{.child{display:none;}}"),
+                $el;
+
+            before("Attaching component to body", function(done){
+                comp.start().then(function(){
+                    return comp.getElement();
+                }).then(function(_$el){
+                    _$el.appendTo("body");
+                    $el = _$el;
+                    done();
+                });
             });
+
+            after(function(done){
+                comp.stop().then(function(){
+                    return comp.getElement();
+                }).then(function(_$el){
+                    _$el.remove();
+                    done();
+                });
+            });
+
+            describe("parent element", function(){
+
+                it("should exist", function(){
+                    expect($el).to.exist;
+                });
+
+                it("should be visible", function(){
+                    expect($el.is(":visible")).to.be.true;
+                });
+
+                it("should be of type div", function(){
+                    expect($el.is("div")).to.be.true;
+                });
+
+            });
+
+            describe("child element", function(){
+
+                it("should exist", function(){
+                    expect($el.find(".child")).to.exist;
+                });
+
+                it("should not be visible", function(){
+                    expect($el.find(".child").is(":visible")).to.be.false;
+                });
+
+                it("should be of type span", function(){
+                    expect($el.find(".child").is("span")).to.be.true;
+                });
+
+                it("should contain the text 'foo'", function(){
+                    expect($el.find(".child").text()).to.be.equal("foo");
+                });
+
+            });
+
         });
 
-        describe("parent element", function(){
+        describe("using option objects", function(){
 
-            it("should exist", function(){
-                expect($el).to.exist;
+            var comp = new UIBase({
+                    html: "<div class='parent'><span class='child'>foo</span></div>"
+                }, {
+                    less: ".parent{.child{display:none;}}"
+                }),
+                $el;
+
+            before("Attaching component to body", function(done){
+                comp.start().then(function(){
+                    return comp.getElement();
+                }).then(function(_$el){
+                    _$el.appendTo("body");
+                    $el = _$el;
+                    done();
+                });
             });
 
-            it("should be visible", function(){
-                expect($el.is(":visible")).to.be.true;
+            after(function(done){
+                comp.stop().then(function(){
+                    return comp.getElement();
+                }).then(function(_$el){
+                    _$el.remove();
+                    done();
+                });
             });
 
-            it("should be of type div", function(){
-                expect($el.is("div")).to.be.true;
+            describe("parent element", function(){
+
+                it("should exist", function(){
+                    expect($el).to.exist;
+                });
+
+                it("should be visible", function(){
+                    expect($el.is(":visible")).to.be.true;
+                });
+
+                it("should be of type div", function(){
+                    expect($el.is("div")).to.be.true;
+                });
+
+            });
+
+            describe("child element", function(){
+
+                it("should exist", function(){
+                    expect($el.find(".child")).to.exist;
+                });
+
+                it("should not be visible", function(){
+                    expect($el.find(".child").is(":visible")).to.be.false;
+                });
+
+                it("should be of type span", function(){
+                    expect($el.find(".child").is("span")).to.be.true;
+                });
+
+                it("should contain the text 'foo'", function(){
+                    expect($el.find(".child").text()).to.be.equal("foo");
+                });
+
             });
 
         });
 
-        describe("child element", function(){
+        describe("using option objects with transforms", function(){
 
-            it("should exist", function(){
-                expect($el.find(".child")).to.exist;
+            var comp = new UIBase({
+                    html: "<div class='parent'><span class='child'>foo</span></div>",
+                    transform: function(data){
+                        // transform the html by adding new elements and
+                        // and returning an actuall DOM element
+                        return when($("<div class='root'>" + data + "<div class='parent-2'></div></div>").get(0));
+                    }
+                }, {
+                    less: ".parent{.child{display:none;}}",
+                    transform: function(data){
+                        // hide the added element
+                        return when(data + ".parent-2{display:none;}");
+                    }
+                }),
+                $root, $parent, $child, $parent2;
+
+            before("Attaching component to body", function(done){
+                comp.start().then(function(){
+                    return comp.getElement();
+                }).then(function(_$el){
+                    _$el.appendTo("body");
+                    $root = _$el;
+                    $parent = $root.find(".parent");
+                    $parent2 = $root.find(".parent-2");
+                    $child = $parent.find(".child");
+                    done();
+                });
             });
 
-            it("should not be visible", function(){
-                expect($el.find(".child").is(":visible")).to.be.false;
+            after(function(done){
+                comp.stop().then(function(){
+                    return comp.getElement();
+                }).then(function(_$el){
+                    _$el.remove();
+                    done();
+                });
             });
 
-            it("should be of type span", function(){
-                expect($el.find(".child").is("span")).to.be.true;
+            describe("root element", function(){
+
+                it("should exist", function(){
+                    expect($root).to.exist;
+                });
+
+                it("should be visible", function(){
+                    expect($root.is(":visible")).to.be.true;
+                });
+
+                it("should be of type div", function(){
+                    expect($root.is("div")).to.be.true;
+                });
+
             });
 
-            it("should contain the text 'foo'", function(){
-                expect($el.find(".child").text()).to.be.equal("foo");
+            describe("parent element", function(){
+
+                it("should exist", function(){
+                    expect($parent).to.exist;
+                });
+
+                it("should be visible", function(){
+                    expect($parent.is(":visible")).to.be.true;
+                });
+
+                it("should be of type div", function(){
+                    expect($parent.is("div")).to.be.true;
+                });
+
+            });
+
+            describe("parent-2 element", function(){
+
+                it("should exist", function(){
+                    expect($parent2).to.exist;
+                });
+
+                it("should not be visible", function(){
+                    expect($parent2.is(":visible")).to.be.false;
+                });
+
+                it("should be of type div", function(){
+                    expect($parent.is("div")).to.be.true;
+                });
+
+            });
+
+            describe("child element", function(){
+
+                it("should exist", function(){
+                    expect($child).to.exist;
+                });
+
+                it("should not be visible", function(){
+                    expect($child.is(":visible")).to.be.false;
+                });
+
+                it("should be of type span", function(){
+                    expect($child.is("span")).to.be.true;
+                });
+
+                it("should contain the text 'foo'", function(){
+                    expect($child.text()).to.be.equal("foo");
+                });
+
             });
 
         });
@@ -96,10 +287,21 @@ describe("UIBase class", function(){
         var comp = new UIBase("/base/test/01.uibase_test.html", "/base/test/01.uibase_test.less"),
             $el;
 
-        before(function(done){
-            comp.getElement().then(function(_$el){
+        before("Attaching component to body", function(done){
+            comp.start().then(function(){
+                return comp.getElement();
+            }).then(function(_$el){
                 _$el.appendTo("body");
                 $el = _$el;
+                done();
+            });
+        });
+
+        after(function(done){
+            comp.stop().then(function(){
+                return comp.getElement();
+            }).then(function(_$el){
+                _$el.remove();
                 done();
             });
         });
